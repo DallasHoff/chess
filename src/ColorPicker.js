c8644fe6e5c4adbf3b9b06935ee661e2dc989f3d
Dallas Hoffman <dallashoff@gmail.com>
2021-08-06 16:31:52 -0400

    Update page's theme-color with color picker

--- src/ColorPicker.js
+++ src/ColorPicker.js
@@ -1,15 +1,29 @@
-import React, { useState, useLayoutEffect } from 'react';
+import React, { useState, useLayoutEffect, useCallback } from 'react';
 import './ColorPicker.scss';
 
 
 export default function ColorPicker({hidden}) {
   const [hue, setHue] = useState(0);
 
-  const changeHue = (newHue) => {
+  const getCssVar = (varName) => getComputedStyle(document.documentElement).getPropertyValue(varName).trim();
+
+  const changeHue = useCallback((newHue) => {
     setHue(newHue);
-    document.documentElement.style.setProperty('--hue', newHue);
     localStorage.themeHue = newHue;
-  };
+    // CSS variable
+    document.documentElement.style.setProperty('--hue', newHue);
+    // Theme color meta tag
+    const themeColor = getCssVar('--tile-dark');
+    let themeColorTag = document.querySelector('meta[name="theme-color"]');
+    if (themeColorTag) {
+      themeColorTag.content = themeColor;
+    } else {
+      themeColorTag = document.createElement('meta');
+      themeColorTag.name = 'theme-color';
+      themeColorTag.content = themeColor;
+      document.head.appendChild(themeColorTag);
+    }
+  }, []);
 
   // Initialize hue from local storage if available or from stylesheet default
   useLayoutEffect(() => {
@@ -17,9 +31,9 @@ export default function ColorPicker({hidden}) {
       const newHue = localStorage.themeHue * 1;
       changeHue(newHue);
     } else {
-      setHue(getComputedStyle(document.documentElement).getPropertyValue('--hue') * 1);
+      setHue(getCssVar('--hue') * 1);
     }
-  }, []);
+  }, [changeHue]);
 
   return (
     <>
